e95666aaa12f05119ef333b90c51c47ab39fba52
Brisppy <49708988+Brisppy@users.noreply.github.com>
2024-10-16 17:45:13 +1100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+# TODO:
+#   pin open-webui version
+#   copy local changes to open-webui
+
+# download required repos
+git clone https://github.com/open-webui/open-webui
+git clone https://github.com/lm-sys/FastChat
+
+# run docker compose
+docker compose up
