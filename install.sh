4dbd1c0aea2b0e5ff67f52552c78cb6e6b208c48
Brisppy <49708988+Brisppy@users.noreply.github.com>
2024-10-19 17:50:29 +1100

    Fix issue cloning specific tags

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ CURDIR="$(basename "$PWD")"
 if [ "$CURDIR" == "rapidanalysis-open-webui" ]; then
   # download required repos
   # repos are pinned to specific releases which are found compatible
-  git clone --single-branch v0.3.32 https://github.com/open-webui/open-webui
-  git clone --single-branch b3923 https://github.com/ggerganov/llama.cpp
+  git clone --branch v0.3.32 https://github.com/open-webui/open-webui
+  git clone --branch b3923 https://github.com/ggerganov/llama.cpp
 
   # replace relevant files with our own versions
   cp "./overwrite/*" "./open-webui"
